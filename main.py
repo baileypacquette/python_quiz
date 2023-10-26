6db8ae271831bcdb5341d279bc4a1c57fa6d3346
bailey pacquette <blackfiredragon2019@gmail.com>
2023-10-26 17:37:18 +0100

    Added a selection feature to main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from game_dictionary import physics_quiz,computer_science_quiz
 if __name__ == '__main__':
 
-    # dictionary of questions where key is the question and answer is the value.
-
-    # ask the user the first question in the dictionary and store their answer.
+    quiz_choice = input('Which quiz would you like to attempt physics,computer science ')
+    if quiz_choice == 'physics':
+        allquestions = list(physics_quiz.keys())
+    if quiz_choice == 'computer':
+        allquestions = list(computer_science_quiz.keys())
+    # ask the user the first question in the dictionary and store their answer.#
     score = 0
-    allquestions = list(physics_quiz.keys())
-
     for i in range(5):
         answer = input((allquestions[i]))
 
