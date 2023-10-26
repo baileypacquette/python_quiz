a56d64a45f760220663607c58363c6293d741e8a
bailey pacquette <blackfiredragon2019@gmail.com>
2023-10-26 17:26:45 +0100

    Added dictionary to seperated files the added to the main

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-
+from game_dictionary import physics_quiz,computer_science_quiz
 if __name__ == '__main__':
 
     # dictionary of questions where key is the question and answer is the value.
-    physics_quiz = {
-        "What is the SI unit of electric current? ": "Ampere",
-        "What is the fundamental particle that carries a negative electric charge?": "Electron",
-        "What force opposes the relative motion of two surfaces in contact?": "Friction",
-        "What property of an object determines its resistance to changes in motion?": "Mass",
-        "What is the SI unit of luminous intensity?": "Candela"
-    }
 
     # ask the user the first question in the dictionary and store their answer.
     score = 0
